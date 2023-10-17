0718dd5fc0f4f2b18b0ac4d7d6ce5e6b3377ba73
DESKTOP-D11U3PH\Dell <thinh.nguyenkiengiang0102@hcmut.edu.vn>
2023-10-17 21:40:20 +0700

    EX4

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -253,7 +253,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 7999;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 9;
+  htim2.Init.Period = 999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -320,15 +320,15 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 int index = 0;
-int counter = 100;
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim )
 {
-		if(index>=4)index=0;
-		counter--;
-		if(counter<=0){
-			counter = 100;
+
+
+	if(index>=4)index=0;
 			update7SEG(index++);
-		}
+
+		HAL_GPIO_TogglePin(GPIOA, DOT_Pin);
 }
 /* USER CODE END 4 */
 
